11dd74b271079dd69281a6a387fda2ec920ba08b
ivanabcastro <ivanabcastro@gmail.com>
2026-07-10 16:11:51 -0400

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,12 @@
 // 2. Build > Realtime Database > Create database (start in test mode for quick setup).
 // 3. Replace every YOUR_* placeholder with your project values.
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD-MOCK_API_KEY_VALUE_143234204662",
+  apiKey: "AIzaSyAnlg3bqtJayS4CuEhgGat1FbH1L4BLzg4",
   authDomain: "bathroom-thoughts.firebaseapp.com",
   databaseURL: "https://bathroom-thoughts-default-rtdb.firebaseio.com",
   projectId: "bathroom-thoughts",
   storageBucket: "bathroom-thoughts.firebasestorage.app",
   messagingSenderId: "143234204662",
-  appId: "1:143234204662:web:5a8e9f2b3c4d5e6f"
-};
-
+  appId: "1:143234204662:web:c8500f2d06dd032c92bdd7",
+  measurementId: "G-HZ28CRBBVE"
 };
